fc2211b5ff5017eedc22bb288a44d152dcb312f6
Никита <afanasyev984@gmail.com>
2021-10-04 20:51:07 +0300

    Добавлены вспомогательные функции (без вызова)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-'use strict';
-
-//Получение случайного целого числа в заданном интервале, включительно, взял с https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-
 function getRandomNumber(min, max) {
+  if (min < 0 || max < 0 || min >= max) {
+    return 'Ошибка! Введенное число меньше нуля, либо числа равны друг другу!';
+  }
   return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+function getRandomFloat(min, max, numberOfDecimal) {
+  if (min < 0 || max < 0 || min >= max) {
+    return 'Ошибка! Введенное число меньше нуля, либо числа равны друг другу!';
+  }
+  let floatNumber =  Math.random() * (max - min + 1) + min;
+  return floatNumber.toFixed(numberOfDecimal);
 }
